b0c7399182cb0f464247e8ebb68fc3e34bbb2898
jennelie01 <coje22ad@student.ju.se>
2023-04-07 13:39:14 +0200

    started with js

--- to-do.js
+++ to-do.js
@@ -1,4 +1,4 @@
-const assignments = [];
-let toDoList = [];
-
-function addToList() {}
+const toDos = [];
+let toDo = getElementById("to-do");
+let listContainer = document.querySelector("list-container");
+let addButton = getElementById("add-button");
