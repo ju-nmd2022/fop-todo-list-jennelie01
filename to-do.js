9f1b3e65227757b4eebac13eb7bc88a9caafd5d5
jennelie01 <coje22ad@student.ju.se>
2023-04-11 22:49:13 +0200

    sources, renaming, json

--- to-do.js
+++ to-do.js
@@ -3,10 +3,12 @@ let toDo = document.getElementById("to-do");
 let itemsList = document.querySelector("#items-list");
 let addButton = document.getElementById("add-button");
 
-addButton.addEventListener("click", addItemToList);
+addButton.addEventListener("click", addTaskToList);
 
-function addItemToList() {
+// function for the task object
+function addTaskToList() {
   const task = {
+    // source for the following line https://www.tutorialspoint.com/javascript/javascript_date_object.htm
     id: new Date().getTime(),
     text: toDo.value,
     done: false,
@@ -15,19 +17,26 @@ function addItemToList() {
   toDos.push(task);
   console.log(toDos);
 
-  addItemToHtml(task);
+  addToDoElement(task);
   // clear input value after it's added
   toDo.value = "";
   saveInLocalStorage();
 }
 
-function addItemToHtml(task) {
+function addToDoElement(task) {
+  // adds div element for task
   const toDoElement = document.createElement("div");
   toDoElement.classList.add("added-task");
 
+  // adds the input text value of the task
   const toDoText = document.createElement("p");
   toDoText.innerHTML = task.text;
   toDoText.classList.add("to-do");
+  if (task.done === true) {
+    toDoText.style.textDecoration = "line-through";
+  }
+
+  // adds p element inside div element
   toDoElement.appendChild(toDoText);
 
   // creates delete button
@@ -40,7 +49,7 @@ function addItemToHtml(task) {
   deleteButton.addEventListener("click", () => {
     itemsList.removeChild(toDoElement);
 
-    // filter returns elements that return true in the following condition
+    // filter returns elements that return true in the following condition, source for method https://www.freecodecamp.org/news/manipulating-arrays-in-javascript/
     // function that keeps task if object id does not match html created element id
     toDos = toDos.filter((toDo) => toDo.id !== task.id); // returns true if not equal
 
@@ -57,7 +66,7 @@ function addItemToHtml(task) {
   checkButton.addEventListener("click", () => {
     toDoText.style.textDecoration = "line-through";
 
-    // map returns all elements with alterations
+    // map returns all elements with alterations, source for method https://www.freecodecamp.org/news/manipulating-arrays-in-javascript/
     // if id's match, return a task with property done = true
     toDos = toDos.map((toDo) => {
       if (toDo.id === task.id) {
@@ -69,10 +78,29 @@ function addItemToHtml(task) {
     saveInLocalStorage();
   });
 
+  // adds that div element on top of the other div elements (tasks)
   itemsList.prepend(toDoElement);
 }
 
+// the following lines about json and forEach was a solution I got help with via chatGPT which I adapted to my code
+// turns object into string
 function saveInLocalStorage() {
   const toDosStringified = JSON.stringify(toDos);
   localStorage.setItem("toDos", toDosStringified);
 }
+
+// checks if to-do exists in storage, parse turns string back into object, and loads them
+function loadToDos() {
+  const toDoStorage = localStorage.getItem("toDos");
+
+  if (toDoStorage) {
+    toDos = JSON.parse(toDoStorage);
+  }
+
+  // each element in the array is called with the function addToDoElement
+  toDos.forEach((toDo) => {
+    addToDoElement(toDo);
+  });
+}
+
+loadToDos();
